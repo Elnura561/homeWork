428666cc12a264732588d7f6fa43e39737bb8ccf
Elnura561 <elnuramametova6@gmail.com>
2024-10-31 11:03:01 +0600

    30.10.2024yaers hw

---
+++ hm30-week9/hw.js
@@ -0,0 +1,11 @@
+var name ="Elnura"
+
+var age = 25
+var isStudent = true
+
+console.log("Аты:" + name  +'/Тип:' ) /* string */
+console.log("Жасы:" + age + "/Тип:") /* number */
+console.log("Стулент пе?" + isStudent + "/Тип:") /* boolean */
+console.log("Жасы: toString" + age + "/Тип:") /* srting */
+
+
--- index/css/js/script.js
+++ index/css/js/script.js
@@ -4,3 +4,34 @@ console.log("ELNURA");
 let text = "сиздин парольниз кате"
 var age = 25
 console.log("жасы: " + age);
+if(true){
+    let age = 20
+    console.log("жасы:" + 20 );
+}
+console.log("жасы:" + age);
+
+var name = "5";
+var age = 15
+var isStudent = false;
+var a;
+if(isStudent){
+    
+}
+console.log("name + age =" + (name+age))
+
+console.log("a дин мани:" + a);
+console.log(typeof name);
+console.log( typeof age);
+
+
+
+
+
+
+
+
+
+
+
+
+
